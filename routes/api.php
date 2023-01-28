95205312868b52c88819384ecd6a109a5bb34c07
pires lufungula <pires00243@gmail.com>
2023-01-28 03:51:53 -0500

    route list

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomerController\Contact\ContactController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+
+
+//CustomerController
+
+Route::prefix('api.orchid-campus')->group(function (){
+
+    //contact form
+    Route::post('/contact-form',[ContactController::class,'ContactUs']);
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
