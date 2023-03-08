d2d55c681ae3cc0e30bf50391bd30af36313c764
pires lufungula <pires00243@gmail.com>
2023-03-08 03:51:38 -0500

    on customer delete cascade user

--- database/migrations/2024_10_12_000000_create_users_table.php
+++ database/migrations/2024_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->id();
             $table->string('user_name')->unique()->index();
             $table->string('email')->unique();
-            $table->foreignIdFor(Customer::class)->constrained();
+            $table->foreignIdFor(Customer::class)->constrained()->onDelete('CASCADE');
             $table->foreignIdFor(Profil::class)->constrained();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
