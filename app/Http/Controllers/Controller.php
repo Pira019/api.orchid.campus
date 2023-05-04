77cf7ff8e062b223a2c7a50bb142ca8a3083e726
pires lufungula <pires00243@gmail.com>
2023-05-04 15:53:27 -0400

    swagger global tag

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,6 +13,7 @@ use Illuminate\Routing\Controller as BaseController;
  *      title="ORCHID REST API",
  *      description="L5 Swagger OpenApi description",
  *      @OA\Contact(
+ *           name="Pires Lufungula",
  *          email="pireslfgl@gmail.com"
  *      ),
  *     @OA\License(
@@ -20,6 +21,16 @@ use Illuminate\Routing\Controller as BaseController;
  *         url="https://www.apache.org/licenses/LICENSE-2.0.html"
  *     )
  * )
+ *
+ *  * @OA\Server(
+     *      url=L5_SWAGGER_CONST_HOST,
+     *      description="Orchid campus Server"
+     * )
+     *
+   * @OA\Tag(
+     *     name="ORCHID CAMPUS PROJECT",
+     *     description="API Endpoints of Projects"
+     * )
  */
 class Controller extends BaseController
 {
