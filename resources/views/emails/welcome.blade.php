eacd93e7e9e0acbc3bc20934018e8e5f6af1ce67
pires lufungula <pires00243@gmail.com>
2023-05-05 00:44:49 -0400

    welcome mail blade text

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,14 @@
+<x-mail::message>
+# Bonjour {{$name}},
+
+Nous sommes ravis de vous accueillir sur notre site de tutoriels pour étudier à l'étranger ! Nous savons que partir à l'étranger pour étudier peut être une expérience excitante et enrichissante, mais cela peut également être intimidant. C'est pourquoi nous avons créé ce site pour vous aider à naviguer dans ce processus et à réussir dans votre parcours éducatif à l'étranger.<br>
+
+Notre site propose des ressources pratiques, des conseils utiles et des astuces pour vous aider à préparer votre voyage d'études à l'étranger. Nous couvrons une variété de sujets allant de la sélection des universités et des programmes d'études, à la recherche de bourses d'études, en passant par la préparation de votre voyage et l'adaptation à une nouvelle culture.<br>
+
+Nous espérons que vous apprécierez votre expérience sur notre site et que vous trouverez toutes les informations dont vous avez besoin pour réussir à étudier à l'étranger. Si vous avez des questions ou des commentaires, n'hésitez pas à nous contacter à l'adresse email indiquée sur notre site.<br>
+
+Nous vous souhaitons beaucoup de succès dans votre parcours d'étude à l'étranger et sommes impatients de vous accompagner tout au long du chemin.<br>
+
+Cordialement,<br>
+L'équipe {{ config('app.name') }}
+</x-mail::message>
