8c9814fe1afda48a54893538f7b4a1db5a84d9fa
pires lufungula <pires00243@gmail.com>
2023-05-19 15:50:30 -0400

    trust host

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -14,6 +14,7 @@ class TrustHosts extends Middleware
     public function hosts()
     {
         return [
+            'orchid-campus.com',
             $this->allSubdomainsOfApplicationUrl(),
         ];
     }
