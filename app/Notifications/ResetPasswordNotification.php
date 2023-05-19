9a15cf145230ca554a10c95857435a0cd98a2e3d
pires lufungula <pires00243@gmail.com>
2023-05-19 15:51:36 -0400

    ResetPasswordNotification class

---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResetPasswordNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(public $url)
+    {
+        $this->afterCommit();
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+        ->from(env("SUPPORT_MAIL_USERNAME"),config('app.name'))
+        ->subject('Réinitialisation de votre mot de passe')
+        ->mailer('support')
+        ->markdown('mail.reset.password',['url'=> $this->url]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
