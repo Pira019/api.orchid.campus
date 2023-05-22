92768b648fe9759fe055a3f2ecfa76c602cd27d2
pires lufungula <pires00243@gmail.com>
2023-05-22 16:48:57 -0400

    update user password service

--- app/Service/UserService.php
+++ app/Service/UserService.php
@@ -4,6 +4,10 @@ namespace App\Service;
 
 use App\Models\User;
 use App\Service\ServiceRessource;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
 
 class UserService extends ServiceRessource{
     public function __contruct(){
@@ -17,6 +21,23 @@ class UserService extends ServiceRessource{
 
     public function userName($name,$idCustomer){
         return substr(strtoupper($name),0,2) . date('Hi') . $idCustomer;
+    }
+
+    /*
+    reset password
+    */
+    public function updatePassword($credentials){
+        $status = Password::reset($credentials,function (User $user, string $password){
+            $user->forceFill([
+                'password' => Hash::make($password)
+            ])->setRememberToken(Str::random(60));
+
+            $user->save();
+
+            event(new PasswordReset($user));
+        });
+
+        return $status;
     }
 }
 
