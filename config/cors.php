86accbe32f47d8a70b9f97e9227f8fa3ed037d7a
pires lufungula <pires00243@gmail.com>
2023-06-07 11:00:34 -0400

    fix orgin

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://www.orchid-campus.com/'],
+    'allowed_origins' => ['orchid-campus.com'],
 
     'allowed_origins_patterns' => [],
 
