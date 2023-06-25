c4fe1853ecf5edb6f4436ec5ec159b30016690e7
pires lufungula <pires00243@gmail.com>
2023-06-25 01:46:09 -0400

    Add coutrySteps features

--- app/Http/Controllers/Api/V1/ManagerController/CountryStepController.php
+++ app/Http/Controllers/Api/V1/ManagerController/CountryStepController.php
@@ -7,11 +7,32 @@ use App\Repository\Manager\CountryStepsRepository;
 
 class CountryStepController extends Controller
 {
-    public function __construct(public CountryStepsRepository $countryStepsRepository){}
+    public function __construct(public CountryStepsRepository $countryStepsRepository)
+    {}
 
-    public function getCountriesToAddTuto(){
+    /**
+     * @OA\Get(
+     *      path="/country-to-add-tuto",
+     *      operationId="getCountryToAddTuto",
+     *      tags={"Add tuto"},
+     *      summary="List countries to add tutoriel or steps",
+     *    @OA\Response(
+     *          response=200,
+     *          description="Successful",
+     *          @OA\JsonContent(
+     *            type="array",
+     *            @OA\Items(ref="#/components/schemas/CountryStepResponse")
+     *          ),
+     *       ),
+     *       @OA\Response(
+     *          response=404,
+     *          description="Bad Request"
+     *      ),
+     *     )
+     */
+    public function getCountryToAddTuto()
+    {
         return $this->countryStepsRepository->getCountriesToAddTuto();
     }
-
 
 }
--- app/Repository/Manager/CountryStepsRepository.php
+++ app/Repository/Manager/CountryStepsRepository.php
@@ -2,17 +2,20 @@
 namespace App\Repository\Manager;
 
 use App\Models\CountryStep;
-use Illuminate\Support\Facades\DB;
 
-class CountryStepsRepository {
+class CountryStepsRepository
+{
 
-   public function  __construct(public CountryStep $countryStep){
+    public function __construct(public CountryStep $countryStep)
+    {
 
     }
     /**
      * get list of country to add new tuto
-    */
-    public function getCountriesToAddTuto(){
-     return  $this->countryStep->rightjoin('countries','country_steps.country_id','=','countries.id')->get();
+     */
+    public function getCountriesToAddTuto()
+    {
+        return $this->countryStep->Rightjoin('countries', 'country_steps.country_id', '=', 'countries.id')
+            ->whereNull('country_steps.country_id')->select('countries.id', 'countries.short_name', 'countries.name')->OrderBy('countries.short_name')->get();
     }
 }
