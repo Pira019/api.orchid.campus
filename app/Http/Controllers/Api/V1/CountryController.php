ef96cb9176e2b5fba519b83e082c6feca7b77c8c
pires lufungula <pires00243@gmail.com>
2023-10-01 03:09:42 -0400

    getCitiesByCountry

--- app/Http/Controllers/Api/V1/CountryController.php
+++ app/Http/Controllers/Api/V1/CountryController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Repository\CountryRepository;
+use Illuminate\Http\Request;
 
 class CountryController extends Controller
 {
     public function __construct(public CountryRepository $countryRepository){}
     public function getList(){
         return $this->countryRepository->getCoutryList(["id","name"]);
+    }
+
+    public function getCitiesByCountry($idCountry,Request $request){
+        $request->merge(['country_id' => $request->route('idCountry')]);
+        $request->validate([
+            'country_id' =>
+                'required|integer',
+        ]);
+        return $this->countryRepository->getCitiesByCountryId($idCountry);
     }
 }
