25958951f5479af522b25c6188e09723d33db1d2
pires lufungula <pires00243@gmail.com>
2023-11-10 00:21:28 -0500

    Revert "Add One To one betwen disciplinarySector and Program"
    This reverts commit b733870658dbd2a64faa0d008f0f54a8fa7824da.

--- app/Models/Program.php
+++ app/Models/Program.php
@@ -4,17 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Program extends Model
 {
     use HasFactory;
-
-    protected $fillable = ['label','disciplinary_sector_id'];
-
-    public function disciplinarySector():HasOne
-    {
-        return $this->HasOne(DisciplinarySector::class);
-    }
 }
