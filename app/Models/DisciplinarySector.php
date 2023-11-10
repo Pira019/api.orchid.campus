250257cfae84f5a1876daeee6a0614103fed6c89
pires lufungula <pires00243@gmail.com>
2023-11-10 00:18:08 -0500

    Add "DisciplinarySectorService" service to handle DisciplinarySector logic

--- app/Models/DisciplinarySector.php
+++ app/Models/DisciplinarySector.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class DisciplinarySector extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+    protected $fillable = ['label', 'description'];
 }
