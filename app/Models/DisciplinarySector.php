4c64fe36b6725467e54b56d0a76c31e4d69615e6
pires lufungula <pires00243@gmail.com>
2023-11-21 08:09:18 -0500

    fix unique program and sector

--- app/Models/DisciplinarySector.php
+++ app/Models/DisciplinarySector.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Core\ServiceUtils;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -16,7 +17,7 @@ class DisciplinarySector extends Model
     protected function label(): Attribute
     {
         return Attribute::make(
-            set: fn (string $value) => ucfirst(strtolower($value)),
+            set: fn (string $value) => ServiceUtils::ucfirst_lower($value),
         );
     }
 }
