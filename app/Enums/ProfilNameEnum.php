4e7751144483f414755e0c06ab6795a4c9c8fae6
pires lufungula <pires00243@gmail.com>
2024-01-30 18:56:46 -0500

    Implit get enum value as array

--- app/Enums/ProfilNameEnum.php
+++ app/Enums/ProfilNameEnum.php
@@ -7,5 +7,10 @@ enum ProfilNameEnum : String
     case MANAGER = "Manager";
     case ADMIN = "Admin";
     case SUPER_ADMIN = "Super-Admin";
+
+    public static function getValues()
+    {
+        return collect(self::cases())->map->value->all();
+    }
 }
 
