debbcad543e3c7fe161bfa28bb8ffe10182ca2dd
pires lufungula <pires00243@gmail.com>
2024-03-16 04:34:41 -0400

    date add

--- app/Http/Requests/Manager/AdmissionRequest.php
+++ app/Http/Requests/Manager/AdmissionRequest.php
@@ -26,7 +26,7 @@ class AdmissionRequest extends FormRequest
         return [
             'detail_program_id' => 'required|integer|exists:university_program,id',
             'type' => 'required|string',
-            'start_at' => 'required|date',
+            'start_at' => 'required|date|after_or_equal:'.date("Y-m-d"),
             'end_at' => 'required|date|after:start_at',
             'session_admission' => 'required|string',
             'link' => 'required|active_url',
@@ -41,11 +41,11 @@ class AdmissionRequest extends FormRequest
     public function attributes(): array
     {
         return [
-            'detail_program_id' => __('id programme'),
+            'detail_program_id' => __('programme id'),
             'start_at' => __('date de début d\'admission'),
             'start_at' => __('date de début d\'admission'),
             'link' => __('lien d\'admission'),
-            'end_at' => __('date fin d\'admission'), 
+            'end_at' => __('date fin d\'admission'),
         ];
     }
 }
