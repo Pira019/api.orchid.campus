7e538e49eef4d77b95883d87e2aa31ea0fb3e354
pires lufungula <pires00243@gmail.com>
2024-03-22 13:44:41 -0400

    Add ServiceController

---
+++ app/Http/Controllers/Api/V1/ManagerController/ServiceController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\ManagerController;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Manager\StoreServiceRequest;
+use Illuminate\Http\Request;
+
+class ServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreServiceRequest $request)
+    {
+        return $request;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
