b051a11648805b53617a939c34084dbfd79fe26f
pires lufungula <pires00243@gmail.com>
2024-04-23 21:51:16 -0400

    Add countriesWithDisciplinaries

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -6,23 +6,45 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Staudenmeir\EloquentHasManyDeep\HasRelationships;
 
 class Country extends Model
 {
     use HasFactory;
+    use HasRelationships;
 
     protected $fillable = ['name', 'short_name'];
-
-    public function countrySteps() : HasMany{
+    
+    public function countrySteps(): HasMany
+    {
         return $this->hasMany(CountryStep::class)->orderBy('order');
     }
 
-    public function cities() : HasMany{
+    public function cities(): HasMany
+    {
         return $this->hasMany(City::class);
     }
 
     public function service(): HasOne
     {
         return $this->hasOne(Service::class);
+    }
+
+    public function programs()
+    {
+        return $this->hasManyDeepFromRelations($this->universities(), (new University())->programs());
+    }
+
+    public function disciplinaries()
+    {
+        return $this->hasManyDeepFromRelations($this->programs(),
+            (new Program())
+                ->disciplineSector(),
+        );
+    }
+
+    public function universities()
+    {
+        return $this->hasManyThrough(University::class, City::class);
     }
 }
